2b1eebab34f14a7b575f977d284b2ebcba146d50
KIDSSCC <18147667093@163.com>
2025-08-13 09:16:34 +0800

    add build.sh

--- script/build.sh
+++ script/build.sh
@@ -10,4 +10,31 @@ script_dir=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
 cd "$script_dir"
 ./gen_parser.sh
 
-build_dir = 
+build_dir="$script_dir/../build"
+cd "$build_dir"
+
+# 初始化一个变量来标记是否包含 -d 选项
+d_flag=false
+
+# 使用 getopts 解析命令行选项
+while getopts "d" opt; do
+    case $opt in
+        d)
+            d_flag=true  # 如果有 -d 选项，则标记为 true
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            exit 1
+            ;;
+    esac
+done
+
+# rm -rf CMakeCache.txt
+rm -rf *log*
+
+if [ "$d_flag" = true ]; then
+    rm -rf miniob
+    # 在这里编写需要在有 -d 选项时执行的操作
+fi
+
+make -j
