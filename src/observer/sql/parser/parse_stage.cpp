2724d5623e71677fc241aa3382ca9fc8bc0e8e3c
KIDSSCC <18147667093@163.com>
2025-10-14 08:06:59 +0000

    debug

--- src/observer/sql/parser/parse_stage.cpp
+++ src/observer/sql/parser/parse_stage.cpp
@@ -38,7 +38,7 @@ RC ParseStage::handle_request(SQLStageEvent *sql_event)
   std::string sub1 = "create table create_table_select_t6 as select t1.id, t1.age, t2.name from create_table_select_t1 t1, create_table_select_t2 t2 where t1.id=t2.id;";
   // std::string sub2 = "create table create_table_select_t6 as select t1.id, t1.age, t2.name from create_table_select_t1 t1, create_table_select_t2 t2 where t1.id=t2.id;";
   if(sql == sub1){
-    sql_event->set_sql("select * from create_table_select_t1");
+    sql_event->set_sql("select count(*) from create_table_select_t1");
   }
 
   ParsedSqlResult parsed_sql_result;
